c87ec79cc8aec4f03bfe29efc8a4aef5242eb12c
rikshitha <113162358+rikshitha@users.noreply.github.com>
2025-04-03 14:39:26 +0530

    Add lib/models/medication.dart 1743671367089

---
+++ lib/models/medication.dart
@@ -0,0 +1,11 @@
+class Medication {
+  final String name;
+  final String dosage;
+  final TimeOfDay time;
+
+  Medication({
+    required this.name,
+    required this.dosage,
+    required this.time,
+  });
+}
