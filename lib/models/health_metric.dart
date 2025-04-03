453b35726446589de40a8d9cf26bff60d400982c
rikshitha <113162358+rikshitha@users.noreply.github.com>
2025-04-03 14:39:27 +0530

    Add lib/models/health_metric.dart 1743671368380

---
+++ lib/models/health_metric.dart
@@ -0,0 +1,11 @@
+class HealthMetric {
+  final DateTime date;
+  final int value;
+  final String metricType;
+
+  HealthMetric({
+    required this.date,
+    required this.value,
+    required this.metricType,
+  });
+}
